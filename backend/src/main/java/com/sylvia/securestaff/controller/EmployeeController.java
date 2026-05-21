503add4a710395a30d4a3d91670a0c26ad5c7000
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-05-21 22:06:06 +0200

    configure CORS for deployment

--- backend/src/main/java/com/sylvia/securestaff/controller/EmployeeController.java
+++ backend/src/main/java/com/sylvia/securestaff/controller/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin(origins = "*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
